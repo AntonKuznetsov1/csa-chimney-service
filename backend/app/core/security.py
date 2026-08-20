702aa586205511ac93ab14e69c5d05c2051f2fa9
Anton Kuznetsov <anton4learning@gmail.com>
2026-08-19 22:18:40 -0300

    Change default admin password

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,7 +3,7 @@ import os
 from fastapi import Header, HTTPException
 
 
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "password").strip()
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "Chimney123").strip()
 
 
 def verify_admin_password(x_admin_password: str = Header(...)):
